3cf1572b7d3fda7d9b78de0ee9bfb1ae90af1ff8
Vinayak Kulkarni <inbox.vinayak@gmail.com>
2020-09-15 12:23:54 +0530

    fix: reordered style & script tags in Vue SFC

--- src/components/CustomDragElement.vue
+++ src/components/CustomDragElement.vue
@@ -5,23 +5,6 @@
     <span class="vue-draggable-handle"></span>
   </span>
 </template>
-<style>
-  .vue-draggable-handle {
-    position: absolute;
-    width: 20px;
-    height: 20px;
-    top: 0;
-    left: 0;
-    background: url("data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg' width='10' height='10'><circle cx='5' cy='5' r='5' fill='#999999'/></svg>")
-      no-repeat;
-    background-position: bottom right;
-    padding: 0 8px 8px 0;
-    background-repeat: no-repeat;
-    background-origin: content-box;
-    box-sizing: border-box;
-    cursor: pointer;
-  }
-</style>
 <script>
   export default {
     name: 'CustomDragElement',
@@ -39,3 +22,20 @@
     },
   };
 </script>
+<style>
+  .vue-draggable-handle {
+    position: absolute;
+    width: 20px;
+    height: 20px;
+    top: 0;
+    left: 0;
+    background: url("data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg' width='10' height='10'><circle cx='5' cy='5' r='5' fill='#999999'/></svg>")
+      no-repeat;
+    background-position: bottom right;
+    padding: 0 8px 8px 0;
+    background-repeat: no-repeat;
+    background-origin: content-box;
+    box-sizing: border-box;
+    cursor: pointer;
+  }
+</style>
