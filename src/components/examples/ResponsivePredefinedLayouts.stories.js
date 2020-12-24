e5c3ea3c2eded311a5f466a97bb2784248ec1c54
Swapnil <swapnil.renge@geospoc.com>
2020-12-24 14:59:48 +0530

    feat: add pre-defined layout example

---
+++ src/components/examples/ResponsivePredefinedLayouts.stories.js
@@ -0,0 +1,6 @@
+import ResponsivePredefinedLayouts from './ResponsivePredefinedLayouts.vue';
+export default { title: 'Examples' };
+export const asAComponentWithResponsivePredefinedLayouts = () => ({
+  components: { ResponsivePredefinedLayouts },
+  template: `<ResponsivePredefinedLayouts/>`,
+});
